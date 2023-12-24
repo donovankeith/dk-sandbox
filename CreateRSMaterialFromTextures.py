4c5086bd6c3834015c439a0eac2cd0844a539444
Donovan Keith <donovanskeith@gmail.com>
2023-12-24 14:57:50 -0800

    Update CreateRSMaterialFromTextures.py

--- CreateRSMaterialFromTextures.py
+++ CreateRSMaterialFromTextures.py
@@ -5,12 +5,14 @@ Description-en-US: Select a texture file to auto-create a Redshift Standard mate
 import c4d
 import maxon
 
+
 def create_material():
     mat = c4d.BaseMaterial(c4d.Mmaterial)
     if mat is None:
         raise ValueError("Unable to create material.")
 
     return mat
+
 
 def create_node_material(node_space_id=None):
     if node_space_id is None:
@@ -30,34 +32,39 @@ def create_node_material(node_space_id=None):
     if graph is None:
         raise ValueError("Unable to add Grap to material.")
 
-    # nimbus_ref = mat.GetNimbusRef(node_space_id)
-    # if nimbus_ref is None:
-    #     raise ValueError("Unable to Get Nimbus Ref.")
-
     root = graph.GetRoot()
     if root is None:
         raise ValueError("Unable to get root node.")
 
     return mat, root
 
+
 def create_redshift_node_material():
     NODE_SPACE_REDSHIFT = "com.redshift3d.redshift4c4d.class.nodespace"
     mat, root = create_node_material(node_space_id=NODE_SPACE_REDSHIFT)
     if (mat is None) or (root is None):
-        raise ValueError("Unable to create redshift node material and retrieve root node.")
+        raise ValueError(
+            "Unable to create redshift node material and retrieve root node.")
 
     return mat, root
 
+
 def main():
-    node_mat, root = create_redshift_node_material()
-    if (node_mat is None) or (root is None):
+    mat, root = create_redshift_node_material()
+    if (mat is None) or (root is None):
         raise ValueError("Unable to create Redshift Material.")
 
     doc.StartUndo()
-    doc.AddUndo(c4d.UNDOTYPE_NEW, node_mat)
-    doc.InsertMaterial(node_mat)
+
+    doc.AddUndo(c4d.UNDOTYPE_NEW, mat)
+    doc.InsertMaterial(mat)
+
+    for child in root.GetChildren():
+        print(child)
+
     doc.EndUndo()
     c4d.EventAdd()
+
 
 if __name__ == "__main__":
     main()
